b967312e2c4ac25469ed6e9438764310ad16249e
Adla <adlagomes@gmail.com>
2024-10-22 13:58:00 -0300

    refatorando codigo

--- app.js
+++ app.js
@@ -21,46 +21,37 @@ let veiculos = []
 const adicionarVeiculoAoDOM = (veiculo) => {
   const vehicleDiv = document.createElement('div')
   vehicleDiv.className ='vehicle'
-  vehicleDiv.innerHTML = `<h3>${veiculo.nome}</h3><p>${veiculo.modelo}</p><p>Guardado em: ${veiculo.garagem}</p>`
+  vehicleDiv.innerHTML = `
+    <h3>${veiculo.nome}</h3>
+    <p>${veiculo.modelo}</p>
+    <p>Guardado em: ${veiculo.garagem}</p>
+  `
   vehicleList.appendChild(vehicleDiv)
 }
 
-form.addEventListener('submit', (e) => {
+const handleFormSubmit = (e) => {
   e.preventDefault()
-
   const nome = nameInput.value
   const modelo = modelInput.value
   const garagem = garageInput.value
-
   const veiculo = new Veiculo(nome, modelo, garagem)
-
   veiculos.push(veiculo)
-  console.log('Carro adicionado:', veiculo);
-  console.log('Lista de carros atualizada:', veiculos)
-
   adicionarVeiculoAoDOM(veiculo)
   form.reset()
-})
+}
 
-showVehiclesButton.addEventListener('click', () => {
+const toggleVehicleList = () => {
   vehicleList.classList.toggle('hidden')
   showVehiclesButton.textContent = vehicleList.classList.contains('hidden') ? 'Mostrar Veículos' : 'Ocultar Veículos'
-})
+}
 
 const buscarVeiculos = () => {
   const termo = searchInput.value.toLowerCase()
-  console.log('termo busca: ', termo)
-
   const resultados = veiculos.filter(veiculo => {
     const nomeVeiculo = veiculo.nome.toLowerCase().includes(termo)
     const nomeGaragem = veiculo.garagem.toLowerCase().includes(termo)
-
-    console.log('Nome:', veiculo.nome.toLowerCase(), 'Garagem:', veiculo.garagem.toLowerCase(), 'Inclui termo:', nomeVeiculo || nomeGaragem);
-        return nomeVeiculo || nomeGaragem;
+    return nomeVeiculo || nomeGaragem;
   })
-
-  console.log('Resultados da busca:', resultados);
-  console.log('Quantidade de resultados:', resultados.length);
 
   searchResults.innerHTML = ''
   searchResults.style.display = 'block'
@@ -69,8 +60,11 @@ const buscarVeiculos = () => {
     resultados.forEach(veiculo => {
       const vehicleDiv = document.createElement('div')
       vehicleDiv.className = 'vehicle'
-      vehicleDiv.innerHTML = `<h3>${veiculo.nome}</h3><p>Modelo: ${veiculo.modelo}</p><p>Garagem: ${veiculo.garagem}</p>`
-      console.log(veiculo.nome)
+      vehicleDiv.innerHTML = `
+        <h3>${veiculo.nome}</h3>
+        <p>Modelo: ${veiculo.modelo}</p>
+        <p>Garagem: ${veiculo.garagem}</p>
+      `
       searchResults.appendChild(vehicleDiv)
     })
   } else {
@@ -78,4 +72,6 @@ const buscarVeiculos = () => {
   }
 }
 
+form.addEventListener('submit', handleFormSubmit)
+showVehiclesButton.addEventListener('click', toggleVehicleList)
 searchButton.addEventListener('click', buscarVeiculos)
